eab55f5c5391bc82a84417a52fb8702cdc8c4561
Kanybek Momukeev <kmomukeev@win.tunduk.kg>
2021-10-01 10:24:12 +0600

    new api additions.
    new api additions.

--- models/migratePostgres.go
+++ models/migratePostgres.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
 
 	log "github.com/sirupsen/logrus"
 
@@ -12,28 +13,35 @@ import (
 )
 
 func MigrateDatabaseUp(path string, migrationsPath string) error {
+	fmt.Printf("\n MigrateDatabaseUp migrationsPath=%s\n", migrationsPath)
 	db, err := sql.Open("postgres", path)
 	if err != nil {
+		fmt.Printf("\n sql.Open ERROR\n")
 		log.WithFields(log.Fields{"omg": err}).Warn("migration error:")
+		return err
 	}
 	defer db.Close()
 
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
+		fmt.Printf("\n postgres.WithInstance ERROR\n")
 		log.WithFields(log.Fields{"omg": err}).Warn("migration error:")
+		return err
 	}
 
-	m, err := migrate.NewWithDatabaseInstance(
-		migrationsPath,
-		"postgres", driver)
+	m, err := migrate.NewWithDatabaseInstance(migrationsPath, "postgres", driver)
 	if err != nil {
+		fmt.Printf("\n migrate.NewWithDatabaseInstance ERROR\n")
 		log.WithFields(log.Fields{"omg": err}).Warn("migration error:")
+		return err
 	}
 
 	err = m.Up()
 
 	if err != nil {
+		fmt.Printf("\n err = m.Up() ERROR\n")
 		log.WithFields(log.Fields{"omg": err}).Warn("migration error:")
+		return err
 	}
 
 	defer m.Close()
@@ -43,24 +51,28 @@ func MigrateDatabaseUp(path string, migrationsPath string) error {
 func MigrateDatabaseDown(path string, migrationsPath string) error {
 	db, err := sql.Open("postgres", path)
 	if err != nil {
+		fmt.Printf("\n sql.Open ERROR\n")
 		log.WithFields(log.Fields{"omg": err}).Warn("migration error:")
+		return err
 	}
-	defer db.Close()
 
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
+		fmt.Printf("\n postgres.WithInstance ERROR\n")
 		log.WithFields(log.Fields{"omg": err}).Warn("migration error:")
+		return err
 	}
 
-	m, err := migrate.NewWithDatabaseInstance(
-		migrationsPath,
-		"postgres", driver)
+	m, err := migrate.NewWithDatabaseInstance(migrationsPath, "postgres", driver)
 	if err != nil {
+		fmt.Printf("\n migrate.NewWithDatabaseInstance ERROR\n")
 		log.WithFields(log.Fields{"omg": err}).Warn("migration error:")
+		return err
 	}
 
 	err = m.Down()
 	if err != nil {
+		fmt.Printf("\n m.Down() ERROR\n")
 		log.WithFields(log.Fields{"omg": err}).Warn("migration error:")
 	}
 
